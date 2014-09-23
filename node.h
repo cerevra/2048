2ebed87acb716c12b691732811151a15df0cb7f8
cerevra <cerevra@gmail.com>
2014-09-23 23:20:42 +0400

    colors correction

--- node.h
+++ node.h
@@ -10,14 +10,15 @@ public:
     ColorHandler()
     {
         m_colors.push_back(QColor(176, 196, 222));// background
-        m_colors.push_back(QColor(162, 162, 109));
-        m_colors.push_back(QColor(158, 144, 105));
-        m_colors.push_back(QColor(139, 139, 94 ));
-        m_colors.push_back(QColor(128, 155, 115));
-        m_colors.push_back(QColor(180, 141, 169));
-        m_colors.push_back(QColor(196, 129, 177));
-        m_colors.push_back(QColor(191, 100, 166));
-        m_colors.push_back(QColor(134, 56 , 112));
+        m_colors.push_back(QColor(74 , 120, 27 ));
+        m_colors.push_back(QColor(188, 137, 2  ));
+        m_colors.push_back(QColor(175, 102, 42 ));
+        m_colors.push_back(QColor(175, 60 , 42 ));
+        m_colors.push_back(QColor(149, 36 , 84 ));
+        m_colors.push_back(QColor(111, 28 , 114));
+        m_colors.push_back(QColor(84 , 33 , 117));
+        m_colors.push_back(QColor(46 , 43 , 122));
+        m_colors.push_back(QColor(31 , 82 , 112));
     }
 
     const QColor& operator[](quint16 index) const
